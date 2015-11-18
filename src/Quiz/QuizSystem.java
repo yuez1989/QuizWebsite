9ed29d539cd63c46238e8469ae83380cf04773d3
Xiaoti Hu <xiaotihool@gmail.com>
2015-11-18 15:54:01 -0800

    Update QuizSystem.java

--- src/Quiz/QuizSystem.java
+++ src/Quiz/QuizSystem.java
@@ -1,14 +1,27 @@
 package Quiz;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class QuizSystem {
 	static public QuizSystem qzsys;
 	static public DataBase db;
+	static public MessageDigest md = null;
+	
 	private QuizSystem(){
 		 db = DataBase.getDataBase();
+		 try {
+			md = MessageDigest.getInstance("SHA");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	static public QuizSystem getQuizSystem(){
 		if(qzsys == null){
@@ -22,5 +35,16 @@ public class QuizSystem {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
 
 		return sdf.format(d);
+	}
+	static public String minusDay(Date in){
+
+		LocalDateTime ldt = LocalDateTime.ofInstant(in.toInstant(), ZoneId.systemDefault());
+		ldt.minusDays(1);
+		Date out = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
+		return sdf.format(out);
+	}
+	static public void destroySystem(){
+		db.disconnectDB();
 	}
 }
