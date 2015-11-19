15377bed187e1dd78a348b2b4a62607869d79f71
yuez1989 <zhangyuebj1989@gmail.com>
2015-11-18 21:21:07 -0800

    we must solve cookie/session issue

--- src/Quiz/SystemListener.java
+++ src/Quiz/SystemListener.java
@@ -17,32 +17,32 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class SystemListener implements ServletContextListener {
 
-    /**
-     * Default constructor. 
-     */
-    public SystemListener() {
+	/**
+	 * Default constructor. 
+	 */
+	public SystemListener() {
 
-		
-        // TODO Auto-generated constructor stub
-    }
+
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    	QuizSystem mysystem = (QuizSystem)arg0.getServletContext().getAttribute("quizsystem");
-    	mysystem.destroySystem();
-    	arg0.getServletContext().removeAttribute("quizsystem");
-    }
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent arg0)  { 
+		// TODO Auto-generated method stub
+		QuizSystem mysystem = (QuizSystem)arg0.getServletContext().getAttribute("quizsystem");
+		mysystem.destroySystem();
+		arg0.getServletContext().removeAttribute("quizsystem");
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0)  { 
-    	QuizSystem mysystem = QuizSystem.getQuizSystem();
-    	arg0.getServletContext().setAttribute("quizsystem", mysystem);
-         // TODO Auto-generated method stub
-    }
-	
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent arg0)  { 
+		QuizSystem mysystem = QuizSystem.getQuizSystem();
+		arg0.getServletContext().setAttribute("quizsystem", mysystem);
+		// TODO Auto-generated method stub
+	}
+
 }
