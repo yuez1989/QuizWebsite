f01d3c96b9f8356da6a59ba1b88072a6e804d24b
yuez1989 <zhangyuebj1989@gmail.com>
2015-11-18 16:09:03 -0800

    Update History.java

--- src/Quiz/History.java
+++ src/Quiz/History.java
@@ -127,12 +127,12 @@ public class History {
 		return QuizSystem.db.executeUpdate(stmt);
 	}
 	
-	public boolean deleteByUserID(String userId){
+	public boolean removeByUserID(String userId){
 		String cmd = "DELETE FROM Histories WHERE usrID = \""+userId+"\";";
 		return QuizSystem.db.executeUpdate(cmd);
 	}
 	
-	public boolean deleteByQuizID(String quizId){
+	public boolean removeByQuizID(String quizId){
 		String cmd = "DELETE FROM Histories WHERE quizID = \""+quizID+"\";";
 		return QuizSystem.db.executeUpdate(cmd);
 	}
