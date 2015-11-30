79ad9c2febc7f52210d191235ac60f771b66faec
Xiaotih <xiaotihool@gmail.com>
2015-11-29 20:06:53 -0800

    Utilities test

--- src/Quiz/Utilities.java
+++ src/Quiz/Utilities.java
@@ -33,7 +33,7 @@ public class Utilities {
 		}
 		for(int i = 0; i<qids.size();i++){
 			History hist = new History(qids.get(i),uids.get(i), endtimes.get(i));
-			
+
 			highScores.add(hist);
 		}
 		return highScores;
@@ -43,23 +43,23 @@ public class Utilities {
 		ArrayList<History> highScores = new ArrayList<History>();
 		String command = "SELECT * FROM Histories WHERE usrID = "+"\""+usrID+"\" ORDER BY score DESC limit 1;";
 		ResultSet rs= db.executeQuery(command);
-		
+
 		ArrayList<String> qids = new ArrayList<String>();
 		ArrayList<String> endtimes = new ArrayList<String>();
-		
+
 		while(rs.next()){
 			qids.add(rs.getString("quizID"));
 			endtimes.add(rs.getString("end"));
 		}
-		
+
 		for(int i = 0; i< qids.size();i++){
 			History hist = new History(qids.get(i),usrID,endtimes.get(i));
 			highScores.add(hist);
 		}	
-		
+
 		return highScores;
 	}
-	
+
 	/**
 	 * Get recent activities from history for every friend of
 	 * a specific user 
@@ -120,7 +120,7 @@ public class Utilities {
 			if(count == 3) break;
 			count++;
 			recentReviews.add(rs.getString("review"));
-			}
+		}
 		return recentReviews;
 	}
 
@@ -169,14 +169,14 @@ public class Utilities {
 		}
 		return recentActs;
 	}
-	
+
 	/**
 	 * Get quizzes recently created by specific user
 	 * @param usrID
 	 * @return Arraylist of quizzes
 	 * @throws SQLException
 	 */
-	public static ArrayList<Quiz> getRecentCreatedQuiz(String usrID) throws SQLException{
+	public static ArrayList<Quiz> s(String usrID) throws SQLException{
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
 		String command = "SELECT * FROM Quizzes WHERE creator = "+"\""+usrID+"\" ORDER BY createTime DESC;";
 		ResultSet rs = db.executeQuery(command);
@@ -187,14 +187,14 @@ public class Utilities {
 		}
 		return quizzes;
 	}
-	
+
 	/**
 	 * Get recent achievements of specific user given his/her ID
 	 * @param usrID
 	 * @return Arraylist of achievement ID
 	 */
 	public static ArrayList<String> getRecentAchievements(String usrID){
-//		ArrayList<String> ach = new ArrayList<String>();
+		//		ArrayList<String> ach = new ArrayList<String>();
 		String command = "SELECT * FROM AchievementRecords WHERE usrID = "+"\""+usrID+"\" ORDER BY time DESC;";
 		ResultSet rs1 = db.executeQuery(command);
 		ArrayList<String> achID = new ArrayList<String>();
@@ -205,21 +205,21 @@ public class Utilities {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-//		for(String achID1 : achID){
-//			String cmd = "SELECT * FROM Achievements WHERE achID = "+"\""+achID1+"\" ;";
-//			ResultSet rs2 = db.executeQuery(cmd);
-//			try {
-//				if(rs2.next()){
-//					ach.add(rs2.getString("description"));
-//				}
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//
-//		}
+		//		for(String achID1 : achID){
+		//			String cmd = "SELECT * FROM Achievements WHERE achID = "+"\""+achID1+"\" ;";
+		//			ResultSet rs2 = db.executeQuery(cmd);
+		//			try {
+		//				if(rs2.next()){
+		//					ach.add(rs2.getString("description"));
+		//				}
+		//			} catch (SQLException e) {
+		//				e.printStackTrace();
+		//			}
+		//
+		//		}
 		return achID;
 	}
-		
+			
 	/**
 	 * Get recent scores of a specific user with userID
 	 * @param usrID
@@ -289,7 +289,7 @@ public class Utilities {
 		}
 		return recentTopPerformance;
 	}
-	
+
 	/**
 	 * Get recent created Quizzes
 	 * @return Arraylist of recent created quizzes in order from newest to oldest
@@ -308,16 +308,16 @@ public class Utilities {
 			return null;
 		}
 	}
-	
-/**
+
+	/**
 	 * Get the top 5% of Quizzes in total played times 
 	 * @return
 	 */
 	static public ArrayList<Quiz> getPopularQuiz(){
 		int count = 0;
 		DataBase db = QuizSystem.getQuizSystem().db;
-//		ResultSet rsCount = db.executeQuery("select COUNT distinct quizID FROM Histories GROUP BY quizID" +
-//				" ORDER BY count(usrID);");
+		//		ResultSet rsCount = db.executeQuery("select COUNT distinct quizID FROM Histories GROUP BY quizID" +
+		//				" ORDER BY count(usrID);");
 		ResultSet rsCount = db.executeQuery("select count(distinct quizID) from Histories;");
 		try {
 			if(rsCount.next()){
@@ -333,13 +333,13 @@ public class Utilities {
 		ResultSet rs = db.executeQuery("SELECT distinct quizID FROM Histories GROUP BY quizID" +
 				" ORDER BY count(usrID) LIMIT " + limit+";");
 		ArrayList<String> quizids = new ArrayList<String>();
-		
+
 		try {
 			while(rs.next()){
 				String qID = rs.getString(1);
 				quizids.add(qID);
 
-				}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -354,8 +354,8 @@ public class Utilities {
 		}
 		return popuQuiz;
 	}
-	
-/**
+
+	/**
 	 * Get the top 5% of players in total played quiz numbers
 	 * @return
 	 * @throws ClassNotFoundException 
@@ -382,41 +382,41 @@ public class Utilities {
 			while(rs.next()){
 				String usrID = rs.getString(1);
 				usrIDs.add(usrID);
-				}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return topUser;
 	}
-	
+
 	/**
 	 * Return average score for all players
 	 * @param quizID
 	 * @return
 	 * @throws SQLException
 	 */
-		public static double getQuizAverageScore(String quizID) throws SQLException{
-			double avg = 0;
-			String command = "SELECT * FROM Histories WHERE quizID = "+"\""+quizID+"\";";
-			ResultSet rs = db.executeQuery(command);
-			int count = 0;
-			double total = 0;
-			while(rs.next()){
-				double score = rs.getDouble("score");
-				total += score;
-				count++;
-			}	
-			avg = total / count;
-			return avg;
-		}
-	
+	public static double getQuizAverageScore(String quizID) throws SQLException{
+		double avg = 0;
+		String command = "SELECT * FROM Histories WHERE quizID = "+"\""+quizID+"\";";
+		ResultSet rs = db.executeQuery(command);
+		int count = 0;
+		double total = 0;
+		while(rs.next()){
+			double score = rs.getDouble("score");
+			total += score;
+			count++;
+		}	
+		avg = total / count;
+		return avg;
+	}
+
 	/**
 	 * Return the highest score for a specific quiz
 	 * @param quizID
 	 * @return score
 	 * @throws SQLException 
 	 */
-	public double getHighestScoreOfQuiz(String quizID) throws SQLException{
+	public static double getHighestScoreOfQuiz(String quizID) throws SQLException{
 		double hscore = 0;
 		String command = "SELECT * FROM Histories WHERE quizID = "+"\""+quizID+"\" ORDER BY score DESC limit 1;";
 		ResultSet rs = db.executeQuery(command);
@@ -425,23 +425,23 @@ public class Utilities {
 		}	
 		return hscore;		
 	}
-		
+
 	/**
 	 * return how many times the quiz has been taken
 	 * @param quizID
 	 * @return
 	 * @throws SQLException 
 	 */
-	public int getPlayTimesOfQuiz(String quizID) throws SQLException{
+	public static int getPlayTimesOfQuiz(String quizID) throws SQLException{
 		int count = 0;
 		DataBase db = QuizSystem.getQuizSystem().db;
 		ResultSet rsCount = db.executeQuery("SELECT COUNT(DISTINCT quizID) FROM Histories;");
-			if(rsCount.next()){
-				count = rsCount.getInt(1);
-			}
+		if(rsCount.next()){
+			count = rsCount.getInt(1);
+		}
 		return count;
 	}
-	
+
 	/**
 	 * Returns list of unread messages of a user
 	 * @param user
@@ -459,7 +459,7 @@ public class Utilities {
 		}
 		return newmsgs;
 	}
-	
+
 	public static ArrayList<String> searchAccounts(String usrID){
 		ArrayList<String> list = new ArrayList<String>();
 		ResultSet rs = QuizSystem.db.executeQuery("Select usrID from Users where usrID like \'%" + usrID+"%\';");
@@ -474,7 +474,7 @@ public class Utilities {
 		}
 		return list;
 	}
-	
+
 	public static ArrayList<String> searchQuizzes(String quizID){
 		ArrayList<String> list = new ArrayList<String>();
 		ResultSet rs = QuizSystem.db.executeQuery("Select quizID from Quizzes where quizID like \'%" + quizID+"%\';");
@@ -489,46 +489,53 @@ public class Utilities {
 		}
 		return list;
 	}
-	
+
 	static public void main(String[] args){
 		DataBase db = QuizSystem.getQuizSystem().db;
-//		DROP TABLE IF EXISTS Quizzes;
-//		CREATE TABLE Quizzes(
-//			quizID VARCHAR(255),
-//			name VARCHAR(1023),
-//			creator VARCHAR(255),
-//			createTime DATETIME,
-//			updateTime DATETIME,
-//			description TEXT,
-//			spec TEXT,
-//			PRIMARY KEY (quizID),
-//			FOREIGN KEY (creator) REFERENCES Users(usrID)
-//		);
-		
-		ArrayList<String> rQuiz = new ArrayList<String>();
-		rQuiz = getRecentQuiz();
-		System.out.print(rQuiz);
-		
-		try {
-			double s = getUserAverageScore("xinhuiwu");
-			System.out.print(s);
+		//		DROP TABLE IF EXISTS Quizzes;
+		//		CREATE TABLE Quizzes(
+		//			quizID VARCHAR(255),
+		//			name VARCHAR(1023),
+		//			creator VARCHAR(255),
+		//			createTime DATETIME,
+		//			updateTime DATETIME,
+		//			description TEXT,
+		//			spec TEXT,
+		//			PRIMARY KEY (quizID),
+		//			FOREIGN KEY (creator) REFERENCES Users(usrID)
+		//		);
 
+//		getRecentAchievements(String usrID)
+//		ArrayList<String> rAch = new ArrayList<String>();
+//		rAch = getRecentAchievements("xiaotihu");//getRecentQuiz();
+		try {
+			System.out.print(getHighestScoreOfQuiz("xiaotihu2015-11-23 19:12:15"));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		
 //		try {
-//			ResultSet rs=db.executeQuery("Select * from Quizzes;");
-//			while(rs.next()){
-//				System.out.println(rs.getString(1));
-//				System.out.println(rs.getString(2));
-//				
-//			}
+//			double s = getUserAverageScore("xinhuiwu");
+//			System.out.print(s);
+//
 //		} catch (SQLException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
+
+		//		try {
+		//			ResultSet rs=db.executeQuery("Select * from Quizzes;");
+		//			while(rs.next()){
+		//				System.out.println(rs.getString(1));
+		//				System.out.println(rs.getString(2));
+		//				
+		//			}
+		//		} catch (SQLException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
 	}
-	
+
 }
